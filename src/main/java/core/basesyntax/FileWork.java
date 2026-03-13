bb2195850fb8adb9fcf3bc0cf87d0d5554548b35
Olena Shtronda <e.shtronda@gmail.com>
2026-03-13 22:32:45 +0200

    complete task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
+    public boolean startWithLetter(String word) {
+        return word.startsWith(SPECIFIED_CHARACTER);
+    }
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            String text = Files.readString(Path.of(fileName)).toLowerCase();
+            String[] words = text.split("[\\s\\p{Punct}]+");
+
+            ArrayList<String> filteredWords = new ArrayList<>();
+
+            for (String word : words) {
+                if (startWithLetter(word)) {
+                    filteredWords.add(word);
+                }
+            }
+
+            String[] result = filteredWords.toArray(new String[0]);
+            Arrays.sort(result);
+
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
